d502977e9044ca1b1fba39f1c8d6dd6bf1a4c698
c86amik <amik.c17@gmail.com>
2021-06-23 10:46:40 +0530

    Initial Commit

---
+++ src/main/java/com/springcavaj/springneo4j/SpringDataNeo4jMasterclassApplication.java
@@ -0,0 +1,13 @@
+package com.springcavaj.springneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataNeo4jMasterclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataNeo4jMasterclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcavaj/springneo4j/SpringDataNeo4jMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcavaj.springneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataNeo4jMasterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
